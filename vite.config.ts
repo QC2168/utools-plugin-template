53b567acf22c45b7e013ef276f67112c40bedcd4
QC2168 <huangxinhong2125@gmail.com>
2023-11-10 15:29:29 +0800

    feat: utools-plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig} from 'vite'
-import utools from 'vite-plugin-utools'
+import { defineConfig } from 'vite'
+import utools from '@qc2168/vite-plugin-utools'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue(), utools([{ entry: './utools/preload.js' }, { entry: './utools/main.js' }])],
+  plugins: [vue(),
+  utools({
+    entry: [
+      { entry: './utools/preload.js' },
+      { entry: './utools/main.js' }],
+    upx: {
+      pluginJsonPath: './public/plugin.json',
+    }
+  })
+  ],
 })
